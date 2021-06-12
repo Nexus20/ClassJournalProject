d1b6d7a97fef611342ff7e52f097f637d8b26f67
Voidwalker20 <chubarov.zheka@gmail.com>
2021-06-12 17:47:38 +0300

    Добавьте файлы проекта.

---
+++ ClassJournalProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ClassJournalProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ClassJournalProject.Areas.Identity.IdentityHostingStartup))]
+namespace ClassJournalProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
